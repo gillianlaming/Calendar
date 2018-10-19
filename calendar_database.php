3c5ac5b55e5a82bfa4e7c972122a385ada9dc440
Leela Ghaemmaghami <leela@leelas-mbp.dhcp.wustl.edu>
2018-10-18 20:07:46 -0500

    revert

--- calendar_database.php
+++ calendar_database.php
@@ -1,6 +1,6 @@
 <?php
 
-  $mysqli = new mysqli('localhost', 'calendar_username', 'calendar_password_hash', 'calendar');
+  $mysqli = new mysqli('localhost', 'calendar_user', 'calendar_pass', 'calendar');
  
   if($mysqli->connect_errno) {
       printf("Connection Failed: %s\n", $mysqli->connect_error);
