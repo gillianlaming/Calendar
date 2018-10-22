8b9acb2ef7fba9fa2edf6639c0b6168291d92238
Gillian Laming <gdlaming@wustl.edu>
2018-10-22 05:23:26 +0200

    minute changes

--- newUser.php
+++ newUser.php
@@ -5,15 +5,19 @@
     }
     if(isset($_POST['username'])){
         $password = $_POST['password'];
-        $salt = random_bytes(5);
-        var_dump(bin2hex($bytes));
+        // $salt = random_bytes(5);
+        // var_dump(bin2hex($bytes));
         $hash = password_hash($password, PASSWORD_BCRYPT); //must encrypt the password
     }
    
     $add_user = $mysqli->prepare("insert into users (username, password_hash) values (?, ?)");
     if(!$add_user){
         printf("Query Prep Failed: %s\n", $mysqli->error);
+        echo("false");
         exit;
+    }
+    else{
+        echo("true");
     }
 
     $add_user->bind_param('ss', $username, $hash);
