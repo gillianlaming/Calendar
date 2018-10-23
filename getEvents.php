eb11692d8fa22630a808e2d3f8ffef11bcd75906
Leela Ghaemmaghami <leela@leelas-mbp.dhcp.wustl.edu>
2018-10-22 22:23:48 -0500

    getEvents.php changed

--- getEvents.php
+++ getEvents.php
@@ -1,15 +1,23 @@
 <?php
     require 'calendar_database.php';
-
+    $user = 'leela'; // change this
     $myArray = array();
-    if ($result = $mysqli->query("select event_id, username, event_name, start_date, end_date, location from events order by event_id")) {
-  
-        while($row = $result->fetch_array(MYSQL_ASSOC)) {
-                $myArray[] = $row;
-        }
-        echo json_encode($myArray);
+    
+    $result = $mysqli->prepare("select event_id, username, event_name, start_date, end_date, location from events where username=?");
+    if(!$result){
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit;
     }
-  
+    $result->bind_param('s', $user);
+    $result->execute();
+
+    $result->bind_result($event_id, $username, $event_name, $start_date, $end_date, $location);
+
+    while ($result->fetch()) {
+        array_push($myArray, ['event_id' => $event_id, 'username' => $username, 'event_name' => $event_name, 'start_date' => $start_date, 'end_date' =>  $end_date, 'location' =>  $location]);
+    }
+    echo json_encode($myArray);
+    
     $result->close();
     $mysqli->close();
 ?>
