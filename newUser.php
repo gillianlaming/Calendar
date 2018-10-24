aac52738f5ec9cd39d34c55237330c7368aefffe
Gillian Laming <gdlaming@wustl.edu>
2018-10-24 04:38:27 +0200

    registration works

--- newUser.php
+++ newUser.php
@@ -1,5 +1,6 @@
 <?php
     require 'calendar_database.php';
+
     if(isset($_POST['username'])){
         $username = $_POST['username'];
     }
@@ -8,26 +9,26 @@
         $hash = password_hash($password, PASSWORD_BCRYPT); 
     }
 
-    $current_users = $mysqli->prepare("select username from users order by username"); // get current users
-        if(!$current_users){
-            printf("Query Prep Failed: %s\n", $mysqli->error);
-            exit;
-        }
-        $current_users->execute();
-        $current_users->bind_result($user); // store usernames in variable
-        $user_exists = true;  
+    // $current_users = $mysqli->prepare("select username from users order by username"); // get current users
+    //     if(!$current_users){
+    //         printf("Query Prep Failed: %s\n", $mysqli->error);
+    //         exit;
+    //     }
+    //     $current_users->execute();
+    //     $current_users->bind_result($user); // store usernames in variable
+    //     $user_exists = true;  
 
-        while($current_users->fetch()){ // loop through all users
-            if ($username === $user){ // check for username
-                $user_exists = false;
-                break;
-            }
-        }
-        $current_users->close();
-        if ($user_exists){
-            echo("false");
-            exit;
-        }
+    //     while($current_users->fetch()){ // loop through all users
+    //         if ($username === $user){ // check for username
+    //             $user_exists = false;
+    //             break;
+    //         }
+    //     }
+    //     $current_users->close();
+    //     if ($user_exists){
+    //         echo("false");
+    //         exit;
+    //     }
    
     $add_user = $mysqli->prepare("insert into users (username, password_hash) values (?, ?)");
     if(!$add_user){
@@ -36,11 +37,12 @@
         exit;
     }
     else{
-        //echo("true");
+        echo("true");
     }
 
     $add_user->bind_param('ss', $username, $hash);
     $add_user->execute();
     $add_user->close();
+    $mysqli->close();
     
 ?>
